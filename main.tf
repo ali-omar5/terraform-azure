3c51bdcf4034b8bac3c800d7131f60601d9f232e
Ali <aaw.omar3@gmail.com>
2026-04-05 17:12:56 +0100

    This is to create a terraform to git upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  required_providers {
+
+    azurerm = {
+
+      source  = "hashicorp/azurerm"
+
+      version = "~> 3.0"
+
+    }
+
+  }
+
+}
+
+provider "azurerm" {
+
+  features {}
+
+}
+
+resource "azurerm_resource_group" "rg" {
+
+  name     = "rg-learning-tf-01"
+
+  location = "uksouth"
+
+}
