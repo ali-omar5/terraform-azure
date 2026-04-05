03a12211dcb61614af0731d595bb0dbbcb516086
Ali <aaw.omar3@gmail.com>
2026-04-05 17:49:03 +0100

    ThisAdd variables and update main.tf to remove hardcoded values

--- main.tf
+++ main.tf
@@ -1,29 +1,18 @@
 terraform {
 
   required_providers {
-
     azurerm = {
-
       source  = "hashicorp/azurerm"
-
       version = "~> 3.0"
-
     }
-
   }
 
 }
-
 provider "azurerm" {
-
   features {}
 
 }
-
 resource "azurerm_resource_group" "rg" {
-
-  name     = "rg-learning-tf-01"
-
-  location = "uksouth"
-
+  name     = var.resource_group_name
+  location = var.location
 }
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "rg-learning-tf-01"
+location            = "uksouth"
