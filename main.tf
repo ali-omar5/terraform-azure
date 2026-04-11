bd185ce613612780667a03553c3c787f07a1debd
Ali <aaw.omar3@gmail.com>
2026-04-11 16:38:16 +0100

    Adding outputs for resources

--- main.tf
+++ main.tf
@@ -53,3 +53,14 @@ resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "subnet_id" {
+value = azurerm_subnet.subnet.id
+}
